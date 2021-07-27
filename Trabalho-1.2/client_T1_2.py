4f9929f27ed243c8fd8bd657f8cbeb91392a85e6
Felippe Mozer <felippemozer22@gmail.com>
2021-07-26 21:16:05 -0300

    Changes on client-side

--- Trabalho-1.2/client_T1_2.py
+++ Trabalho-1.2/client_T1_2.py
@@ -2,13 +2,14 @@ import xmlrpc.client
 from time import time
 from random import randint
 
-def run(value):
-    keylist = list(range(value))
+def run(m):
+    keylist = list(range(m))
     start = time()
 
     proxy = xmlrpc.client.ServerProxy("http://localhost:8000/")
     multicall = xmlrpc.client.MultiCall(proxy)
-    for v in value:
-        keylist[v] = multicall.put(v)
+    for v in m:
+        value = randint(0, m)
+        keylist[v] = multicall.put(value)
     
     end = time() - start
