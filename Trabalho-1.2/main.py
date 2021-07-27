559ca8bf567768d216305f581d833d524f8668d2
Leonardo Ugiete <leonardougiete@hotmail.com>
2021-07-26 22:14:42 -0300

    merge main

--- Trabalho-1.2/main.py
+++ Trabalho-1.2/main.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
 from src.server import Server
-from src.client import Client
-import asyncio
 
 s = Server(port=9999)
-=======
-import client_T1_2 as client
-
-m = 10
-client.run(m)
->>>>>>> 73c31ef31ec06ea955e4731a3f0d9b50f89629eb
